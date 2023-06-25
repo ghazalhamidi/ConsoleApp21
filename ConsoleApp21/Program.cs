76daf3bcb7da4c0ea9271c5b46b811df1f78ee0a
Partiran <Partiran@DESKTOP-IR2LBIU>
2023-06-25 19:39:26 +0330

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            calc(x);
+
+        }//end main
+
+        private  static void calc(int x)
+        {
+            double result = (Math.Pow(x, 2) + (2 * x) - 4);
+            Console.WriteLine(result);
+
+        }
+    }
+}
